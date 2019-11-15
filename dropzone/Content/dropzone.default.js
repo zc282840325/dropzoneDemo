2e3e1ca37b6180396ee6ba636e6155ec81687fa5
zc282840325 <282840325@qq.com>
2019-11-15 13:41:10 +0800

    1

---
+++ dropzone/Content/dropzone.default.js
@@ -0,0 +1,35 @@
+﻿+function () {
+    if (!window.Dropzone) {
+        return;
+    }
+
+    var options = {
+        paramName: "file",
+        dictDefaultMessage: "拖放文件上传",
+        dictFallbackMessage: "您的浏览器不支持拖放文件上传",
+        dictFallbackText: "您的浏览器不支持拖放文件上传,请使用表单上传文件",
+        dictFileTooBig: "文件太大了 ({{filesize}}MiB). 文件大小: {{maxFilesize}}MiB.",
+        dictInvalidFileType: "文件类型错误!",
+        dictResponseError: "服务端返回 {{statusCode}} .",
+        dictCancelUpload: "取消文件上传",
+        dictUploadCanceled: "文件上传已取消",
+        dictCancelUploadConfirmation: "确定取消上传操作吗?",
+        dictRemoveFile: "移除文件",
+        dictMaxFilesExceeded: "最多只能上传 {{maxFiles}} 个文件",
+        // 移除文件操作链接
+        addRemoveLinks: true,
+        headers: {},
+        maxfilesexceeded: function (file) {
+            this.removeFile(file);
+            abp.message.warn("最多只能上传 " + this.options.maxFiles + " 个文件");
+        }
+    };
+
+    var token = abp.security.antiForgery.getToken();
+    if (token) {
+        options.headers[abp.security.antiForgery.tokenHeaderName] = token;
+    }
+
+    Dropzone.extend(Dropzone.prototype.defaultOptions, options);
+
+}()
---
+++ dropzone/Controllers/HomeController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dropzone.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UpLoadProcess(HttpPostedFileBase imgFile)
+        {
+            Hashtable hash = new Hashtable();
+            if (Request.Files.Count <= 0)
+            {
+                hash = new Hashtable();
+                hash["success"] = false;
+                hash["message"] = "Please select a file.";
+                return Json(hash);
+            }
+            imgFile = Request.Files[0];
+            string fileTypes = "gif,jpg,jpeg,png,bmp";
+            string fileName = imgFile.FileName;
+            string fileExt = Path.GetExtension(fileName).ToLower();
+
+            if (string.IsNullOrEmpty(fileExt) || Array.IndexOf(fileTypes.Split(','), fileExt.Substring(1).ToLower()) == -1)
+            {
+                hash = new Hashtable();
+                hash["success"] = false;
+                hash["message"] = "Upload file extension is an extension that is not allowed.";
+                return Json(hash);
+            }
+
+            string filePathName = string.Empty;
+            string localPath = Path.Combine(HttpRuntime.AppDomainAppPath, "Resource");
+            if (Request.Files.Count == 0)
+            {
+                return Json(new
+                {
+                    success = false,
+                    message = "Faile"
+                });
+            }
+            string ex = Path.GetExtension(imgFile.FileName);
+            filePathName = Guid.NewGuid().ToString("N") + ex;
+            if (!System.IO.Directory.Exists(localPath))
+            {
+                System.IO.Directory.CreateDirectory(localPath);
+            }
+            imgFile.SaveAs(Path.Combine(localPath, filePathName));
+            return Json(new
+            {
+                success = true,
+                message = "/Resource" + "/" + filePathName
+            });
+
+        }
+        //[HttpPost]
+        //public async Task<JsonResult> UploadFile(string defaultFileUploadTextInput)
+        //{
+        //    try
+        //    {
+        //        var file = this.Request.Files.First();
+
+        //        //Check input
+        //        if (file == null)
+        //        {
+        //            throw new UserFriendlyException("请选择一个文件!");
+        //        }
+
+        //        if (file.Length > 1048576) //1MB
+        //        {
+        //            throw new UserFriendlyException("文件太大!");
+        //        }
+
+        //        byte[] fileBytes;
+        //        using (var stream = file.OpenReadStream())
+        //        {
+        //            fileBytes = stream.GetAllBytes();
+        //        }
+
+        //        var fileObject = new BinaryObject(fileBytes);
+        //        await this._binaryObjectManager.SaveAsync(fileObject);
+
+        //        return this.Json(new AjaxResponse(new
+        //        {
+        //            id = fileObject.Id,
+        //            contentType = file.ContentType,
+        //            defaultFileUploadTextInput = defaultFileUploadTextInput
+        //        }));
+        //    }
+        //    catch (UserFriendlyException ex)
+        //    {
+        //        return this.Json(new AjaxResponse(new ErrorInfo(ex.Message)));
+        //    }
+        //}
+
+        //public async Task<ActionResult> GetFile(Guid id, string contentType)
+        //{
+        //    var fileObject = await this._binaryObjectManager.GetOrNullAsync(id);
+        //    if (fileObject == null)
+        //    {
+        //        return this.StatusCode((int)HttpStatusCode.NotFound);
+        //    }
+
+        //    return this.File(fileObject.Bytes, contentType);
+        //}
+        // }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
